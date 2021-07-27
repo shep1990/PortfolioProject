07ce3c91c36d858175a08f023e166386ea5c048f
Richard Shepherd <richardjohn90@hotmail.co.uk>
2021-07-27 15:11:51 +0100

    removed files included in template

--- PortfolioProject.Web/ClientApp/src/app/app.module.ts
+++ PortfolioProject.Web/ClientApp/src/app/app.module.ts
@@ -2,13 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { PortfolioService } from './services/portfolio.service';
@@ -17,9 +13,6 @@ import { PortfolioService } from './services/portfolio.service';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     PortfolioComponent
   ],
   imports: [
